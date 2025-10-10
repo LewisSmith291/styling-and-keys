255de4e26f1c864164ef30e63e9259674fcd0db1
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-10 06:19:45 +0100

    Added components to render list of bots

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import useState from 'react';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BotListManager />
     </div>
   );
 }
 
 function BotListManager(){
-  const [bots, setBots] = iseState([
-    {id: 1, name: "Send Email", status: "Running", task: "Sending"},
-    {id: 2, name: "Send Notifications"},
-    {}
-  ])
+  const [bots, setBots] = useState([
+    {id: 1, name: "Email manager", status: "Running", task: "Send/read emails"},
+    {id: 2, name: "Notification manager", status: "Completed", task:"Send notifications"},
+    {id: 3, name: "Data analyzer", status: "Stopped", task: "Analyze data"}
+  ]);
+
+  return (<GetBotList list={bots}/>);
+}
+
+function GetBotList(props){
+  const list = props.list.map((bot) => 
+  <li className="bot" key={bot.id}>
+    <div className="bot-name">Name: {bot.name}</div>
+    <div className="bot-status">Status: {bot.status}</div>
+    <div className="bot-task">Task: {bot.task}</div>
+  </li>
+  )
+  return (<ul id="bot-list">{list}</ul>);
 }
 
 export default App;
