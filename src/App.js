7fe85dc42b3fb756ddc35faabee17c0996d41a1b
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-10 07:06:22 +0100

    Implemented list to display all bots. Styled the list items

--- src/App.js
+++ src/App.js
@@ -25,9 +25,19 @@ function GetBotList(props){
     <div className="bot-name">Name: {bot.name}</div>
     <div className="bot-status">Status: {bot.status}</div>
     <div className="bot-task">Task: {bot.task}</div>
+    <button>Remove Bot</button>
   </li>
   )
-  return (<ul id="bot-list">{list}</ul>);
+  return (
+    <ul id="bot-list">
+      <li key="headers" className='bot'>
+        <div className='header'><b>Name</b></div>
+        <div className='header'><b>Status</b></div>
+        <div className='header'><b>Task</b></div>
+        <div className='header'></div>
+      </li>
+      {list}
+    </ul>);
 }
 
 export default App;
