d0bd31bb49bb84707642e19c328fdf41767a3818
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-11 14:15:38 +0100

    Added colouring to status of bot depending on the type of status

--- src/App.js
+++ src/App.js
@@ -19,25 +19,42 @@ function BotListManager(){
   return (<GetBotList list={bots}/>);
 }
 
+function GetStatusColour(bot){
+  if (bot.status === "Running"){
+  return (<div className="bot-status running">{bot.status}</div>)
+  }
+  else if (bot.status === "Completed"){
+  return (<div className="bot-status completed">{bot.status}</div>)
+  }
+  else if (bot.status === "Stopped"){
+  return (<div className="bot-status stopped">{bot.status}</div>)
+  }
+
+}
+
 function GetBotList(props){
   const list = props.list.map((bot) => 
   <li className="bot" key={bot.id}>
-    <div className="bot-name">Name: {bot.name}</div>
-    <div className="bot-status">Status: {bot.status}</div>
-    <div className="bot-task">Task: {bot.task}</div>
+    <div className="bot-id">{bot.id}</div>
+    <div className="bot-name">{bot.name}</div>
+    <GetStatusColour status = {bot.status}/>
+    <div className="bot-task">{bot.task}</div>
     <button>Remove Bot</button>
   </li>
-  )
+  );
+
   return (
     <ul id="bot-list">
       <li key="headers" className='bot'>
-        <div className='header'><b>Name</b></div>
-        <div className='header'><b>Status</b></div>
-        <div className='header'><b>Task</b></div>
+        <div className='header'>ID</div>
+        <div className='header'>Name</div>
+        <div className='header'>Status</div>
+        <div className='header'>Task</div>
         <div className='header'></div>
       </li>
       {list}
-    </ul>);
+    </ul>
+  );
 }
 
 export default App;
