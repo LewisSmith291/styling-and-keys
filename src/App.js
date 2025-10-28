ad06979e85488e9f5b31910d18ec6b8d334a6b53
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-28 15:22:59 +0000

    Started coding remove bot button. The button removes the bots from the bot list but the UI is not updated straight away.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ const [bots, setBots] = useState([
     }
 
     const addBot = (event) => {
+      event.preventDefault();
+      if (newBotType==="") return;
       // Getting ID
       let newID;
       let highestId = 0;
@@ -38,7 +40,6 @@ const [bots, setBots] = useState([
       tempBotList.push(newBot);
       setBots(tempBotList);
       setNewBotType("");
-      event.preventDefault();
     }
 
     const handleChange = (event) => {
@@ -62,6 +63,79 @@ const [bots, setBots] = useState([
     return (<GetBotList list={bots}/>);
   }
 
+  function GetBotList(props){
+    const [botList, setBotList] = useState(
+      props.list.map((bot) => 
+        <li className="bot" key={bot.id}>
+          <div className="bot-id">{bot.id}</div>
+          <div className="bot-name">{bot.name}</div>
+          <GetStatusColour status = {bot.status}/>
+          <div className="bot-task">{bot.task}</div>
+          <StartJobButton 
+            name = {bot.name}
+            status = {bot.status}
+          />
+          <RemoveBotButton 
+            bot = {bot}
+            />
+        </li>
+    ));
+
+    function GetStatusColour(bot){
+      if (bot.status === "Running"){
+      return (<div className="bot-status running">{bot.status}</div>)
+      }
+      else if (bot.status === "Completed"){
+      return (<div className="bot-status completed">{bot.status}</div>)
+      }
+      else if (bot.status === "Awaiting"){
+      return (<div className="bot-status awaiting">{bot.status}</div>)
+      }
+    }
+
+    function StartJobButton(bot){
+      const [jobTimer, setJobTimer] = useState(0);
+      const [jobStatus, setJobStatus] = useState(bot.status);
+      if (bot.name === "Email manager"){
+
+      }
+      else if (bot.name === ""){
+
+      }
+
+      return(
+        <button className="start-job bot-button">Start Job</button>
+      )
+    }
+
+    function RemoveBotButton(bot){
+      function RemoveBot(){
+        let tempBotList = botList;
+        let index = tempBotList.indexOf(bot.bot);
+        tempBotList.splice(index,1);
+        setBotList(tempBotList);
+      }
+
+      return(
+        <button onClick={RemoveBot} className="remove-bot bot-button">Remove Bot</button>
+      )
+    }
+
+    return (
+      <ul id="bot-list">
+        <li id="header-id" key="headers" className='bot'>
+          <div className='header bot-id'>ID</div>
+          <div className='header'>Name</div>
+          <div className='header'>Status</div>
+          <div className='header'>Task</div>
+          <div className='header'></div>
+        </li>
+        {botList}
+      </ul>
+    );
+  }
+
+
 
   return (
     <div>
@@ -71,74 +145,7 @@ const [bots, setBots] = useState([
   )
 }
 
-function GetBotList(props){
-  const list = props.list.map((bot) => 
-  <li className="bot" key={bot.id}>
-    <div className="bot-id">{bot.id}</div>
-    <div className="bot-name">{bot.name}</div>
-    <GetStatusColour status = {bot.status}/>
-    <div className="bot-task">{bot.task}</div>
-    <StartJobButton 
-      name = {bot.name}
-      status = {bot.status}
-    />
-    <RemoveBotButton 
-      bot = {bot}
-      />
-  </li>
-  );
 
-  function GetStatusColour(bot){
-    if (bot.status === "Running"){
-    return (<div className="bot-status running">{bot.status}</div>)
-    }
-    else if (bot.status === "Completed"){
-    return (<div className="bot-status completed">{bot.status}</div>)
-    }
-    else if (bot.status === "Awaiting"){
-    return (<div className="bot-status awaiting">{bot.status}</div>)
-    }
-  }
-
-  function StartJobButton(bot){
-    const [jobTimer, setJobTimer] = useState(0);
-    const [jobStatus, setJobStatus] = useState(bot.status);
-    if (bot.name === "Email manager"){
-
-    }
-    else if (bot.name === ""){
-
-    }
-
-    return(
-      <button className="start-job bot-button">Start Job</button>
-    )
-  }
-
-  function RemoveBotButton(bot){
-    function RemoveBot(){
-      // Needs to remove the bot from the list
-      // the list will update on the screen
-    }
-
-    return(
-      <button onClick={RemoveBot} className="remove-bot bot-button">Remove Bot</button>
-    )
-  }
-
-  return (
-    <ul id="bot-list">
-      <li id="header-id" key="headers" className='bot'>
-        <div className='header bot-id'>ID</div>
-        <div className='header'>Name</div>
-        <div className='header'>Status</div>
-        <div className='header'>Task</div>
-        <div className='header'></div>
-      </li>
-      {list}
-    </ul>
-  );
-}
 
 
 export default App;
